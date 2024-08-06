b173e31b292c8675491372695f1e03efd98a84a8
jaquelineatienza <jaquibatienza.com>
2024-08-06 11:51:30 -0300

    trabajo  terminado

--- src/controllers/taks.controllers.js
+++ src/controllers/taks.controllers.js
@@ -8,7 +8,12 @@ crtl.obtenerTaks = async ( req,res)=>{
         const conexion = await conectDB();
 
     const [resultado] = await conexion.query('SELECT * FROM tasks');
-    return res.json(resultado);
+    if(resultado.length === 0){
+        res.status(204).json({msg:'La tarea no se encuentra registrada en la base de datos'})
+    }else{
+        return res.json(resultado);
+    }
+  
     }catch(error){
         res.status(500).json({msg:'error interno del servidor'})
     }
@@ -22,7 +27,7 @@ crtl.obtenerID = async ( req,res)=>{
 
         const [resultado]= await conexion.query('SELECT * FROM tasks  WHERE id = ?',[id]);
         if(resultado.length === 0){
-            res.status(404).json({msg:'La tarea no se encuentra registrada en la base de datos'})
+            res.status(204).json({msg:'La tarea no se encuentra registrada en la base de datos'})
         }else{
             
         return res.status(200).json(resultado);
@@ -41,11 +46,13 @@ crtl.crearTaks = async ( req,res)=>{
         await conexion.query('INSERT INTO  tasks ( title, description, isComplete) VALUES (?,?,?)',[
             title,description,isComplete
         ]) ;
-    
-        //respuesta
-        res.json({
-            msg:'tarea creada'
-        })
+        if(title === " " ||description === " "||isComplete ===" " ){
+            res.json({msg:'todos los campos son requeridos'})
+        }else{
+            res.json({
+                msg:'tarea creada'
+            })
+        }      
     }catch(error){
         res.status(500).json({msg:'error interno del servidor',error})
     }
@@ -65,6 +72,8 @@ crtl.editarTaks = async(req,res)=>{
         const [resultado] = await conexion.query('UPDATE tasks SET title = ?, description = ?, isComplete = ? WHERE id = ?', [title, description, isComplete, id]);
         if(!resultado.ok){
          return res.status(200).json({msg:'la tarea fue editada correctamente'})
+        }else{
+            return res.status(404).json({msg:'ocurrio un error al editar la tarea '})
         }
     }
    
@@ -78,8 +87,10 @@ crtl.eliminarTaks = async(req,res)=>{
     const {id} = req.params;
     try{
         const [resultado]= await conexion.query('DELETE FROM `tasks` WHERE id = ?',[id]);
-      
-            res.status(200).json({msg:'tarea eliminada correctamente'});
+            if(!resultado.ok){
+                res.status(200).json({msg:'tarea eliminada correctamente'});
+            }
+           
         
     }catch(error){
         res.status(500).json({msg:'error interno del servidor'})
